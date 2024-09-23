2520e287a063fa13a2123029521134b7a9f9f4a4
AlessioMontebello90 <alessio.montebello.am@gmail.com>
2024-09-23 16:28:16 +0200

    add dynamic components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
-import reactImg from './assets/react-core-concepts.png';
 import { CORE_CONCEPTS } from './data.js';
-import Header from './components/Header.jsx';
+import Header from './components/Header/Header.jsx';
+import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
 
-function CoreConcept({ image, title, description }) {
-  return ( 
-  <li>
-    <img src={image} alt={title} />
-    <h3>{title}</h3>
-    <p>{description}</p>
-  </li>
-  );
-}
+
 
 function App() {
+  function handleSelect(selectedButton) {
+    console.log(selectedButton);
+}
   return (
     <div>
       <Header />
@@ -30,6 +26,16 @@ function App() {
           <CoreConcept {...CORE_CONCEPTS[3]} />
 
         </ul>
+        </section>
+          <section id="examples">
+            <h2>Examples</h2>
+            <menu>
+              <TabButton onSelect={() => handleSelect ('components')}>Components</TabButton>
+              <TabButton onSelect={() => handleSelect ('jsx')}>JSX</TabButton>
+              <TabButton onSelect={() => handleSelect ('props')}>Props</TabButton>
+              <TabButton onSelect={() => handleSelect ('state')}>State</TabButton>
+            </menu>
+            
         </section>
       </main>
     </div>
