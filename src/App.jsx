922f8d84196185f53dff6ed6572423ab5df9a27f
AlessioMontebello90 <alessio.montebello.am@gmail.com>
2024-09-23 11:40:35 +0200

    update obj

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import reactImg from './assets/react-core-concepts.png';
-import componentsImg from './assets/components.png';
+import { CORE_CONCEPTS } from './data.js'
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function genRandomInt(max) {
@@ -35,15 +35,25 @@ function App() {
         <section id='core-concepts'>
         <h2>Time to get started!</h2>
         <ul>
-         <CoreConcept title='Components'
-          description="The Core UI building block."
-          img={componentsImg} />
+         <CoreConcept title={CORE_CONCEPTS[0].title}
+          description={CORE_CONCEPTS[0].description}
+          img={CORE_CONCEPTS[0].image} />
 
-         <CoreConcept title='Proprs' />
+           <CoreConcept title={CORE_CONCEPTS[1].title}
+          description={CORE_CONCEPTS[1].description}
+          img={CORE_CONCEPTS[1].image} />
 
-         <CoreConcept title='' />
+           <CoreConcept title={CORE_CONCEPTS[2].title}
+          description={CORE_CONCEPTS[2].description}
+          img={CORE_CONCEPTS[2].image} />
 
-         <CoreConcept title='' />
+           <CoreConcept title={CORE_CONCEPTS[3].title}
+          description={CORE_CONCEPTS[3].description}
+          img={CORE_CONCEPTS[3].image} />
+
+          
+
+      
         </ul>
         </section>
       </main>
