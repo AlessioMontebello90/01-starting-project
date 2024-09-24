555515941999caa46220ac6283757f71cf501e69
AlessioMontebello90 <alessio.montebello.am@gmail.com>
2024-09-24 14:39:37 +0200

    small fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,56 +3,58 @@ import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
-import {EXAMPLES} from './data.js';
-
-
+import { EXAMPLES } from './data.js'; 
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState();
 
-  const [ selectedTopic, setSelectedTopic ] = useState('components');
-  
-  
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
     // console.log(selectedTopic);
-}
+  }
 
-console.log('APP COMPONENT EXECUTING');
+  console.log('APP COMPONENT EXECUTING');
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
+
   return (
     <div>
       <Header />
       <main>
-        <section id='core-concepts'>
-        <h2>Time to get started!</h2>
-        <ul>
-         <CoreConcept 
-         title={CORE_CONCEPTS[0].title}
-          description={CORE_CONCEPTS[0].description}
-          img={CORE_CONCEPTS[0].image} />
-
-          <CoreConcept {...CORE_CONCEPTS[1]} />
-          <CoreConcept {...CORE_CONCEPTS[2]} />
-          <CoreConcept {...CORE_CONCEPTS[3]} />
-
-        </ul>
+        <section id="core-concepts">
+          <h2>Time to get started!</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              img={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
         </section>
-          <section id="examples">
-            <h2>Examples</h2>
-            <menu>
-              <TabButton onSelect={() => handleSelect ('components')}>Components</TabButton>
-              <TabButton onSelect={() => handleSelect ('jsx')}>JSX</TabButton>
-              <TabButton onSelect={() => handleSelect ('props')}>Props</TabButton>
-              <TabButton onSelect={() => handleSelect ('state')}>State</TabButton>
-            </menu>
-            <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>
-              {EXAMPLES[selectedTopic].code}
-              </code>
-            </pre>
-            </div>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+          </menu>
+          {tabContent}
         </section>
       </main>
     </div>
--- src/components/CoreConcept.jsx
+++ src/components/CoreConcept.jsx
@@ -1,4 +1,4 @@
-export default 
+export default CoreConcept
 
 function CoreConcept({ image, title, description }) {
     return ( 
